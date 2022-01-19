5672302fb70d0a3ace01863e2f552b21a60d8d43
Emerson Costa <ercosta90@gmail.com>
2022-01-18 23:04:19 -0300

    Exercicio if-else

---
+++ ws_eclipse/ex10/src/ex10.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ex10 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int horaInicio, horaFinal;
+		
+		horaInicio = sc.nextInt();
+		horaFinal = sc.nextInt();
+		
+		if (horaFinal <= horaInicio) {
+			horaFinal += 24; 
+		} 
+		
+		System.out.printf("O JOGO DUROU %d HORA(S)", horaFinal - horaInicio);
+	}
+
+}
