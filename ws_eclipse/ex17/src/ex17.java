3bd50144ebd233a0e578d8b9dca37003be05caf9
Emerson Costa <ercosta90@gmail.com>
2022-01-20 23:00:27 -0300

    Exercicio 17

--- ws_eclipse/ex17/src/ex17.java
+++ ws_eclipse/ex17/src/ex17.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class ex17 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int codigo, alcool, gasolina, diesel;
+		
+		codigo = sc.nextInt();
+		alcool = 0;
+		gasolina = 0;
+		diesel = 0;
+		while (codigo != 4) {
+			codigo = sc.nextInt();
+			if (codigo == 1) {
+				alcool += 1;
+			} else if (codigo == 2) {
+				gasolina += 1;
+			} else if (codigo == 3) {
+				diesel += 1;
+			}
+		}
+		System.out.println("MUITO OBRIGADO");
+		System.out.printf("Alcool: %d%n", alcool);
+		System.out.printf("Gasolina: %d%n", gasolina);
+		System.out.printf("Diesel: %d%n", diesel);
 	}
 
 }
