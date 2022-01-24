babdc5137311e4dcb0d4058da194e88fbc225239
Emerson Costa <ercosta90@gmail.com>
2022-01-24 00:08:34 -0300

    Classes

--- ws_eclipse/NotaFinal/src/applicaton/Program.java
+++ ws_eclipse/NotaFinal/src/applicaton/Program.java
@@ -1,9 +1,35 @@
 package applicaton;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.NotaFinal;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		NotaFinal notafinal = new NotaFinal();
+		
+		System.out.print("Entre com o nome do aluno(a): ");
+		notafinal.name = sc.nextLine();
+		System.out.print("Entre com a nota do primeiro trimestre: ");
+		notafinal.nPrimeiroTrimestre = sc.nextDouble();
+		System.out.print("Entre com a nota do segundo trimestre: ");
+		notafinal.nSegundoTrimestre = sc.nextDouble();		
+		System.out.print("Entre com a nota do terceiro trimestre: ");
+		notafinal.nTerceiroTrimestre = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("RESULTADO FINAL");
+		System.out.println("NAME: " + notafinal.nomeAluno());
+		System.out.printf("FINAL GRADE: %.2f%n", notafinal.resultadoFinal());
+		System.out.println("STATUS: " + notafinal.toString());
+		
+		
 
 	}
 
--- ws_eclipse/NotaFinal/src/entities/NotaFinal.java
+++ ws_eclipse/NotaFinal/src/entities/NotaFinal.java
@@ -1,5 +1,28 @@
 package entities;
 
 public class NotaFinal {
-
+	
+	public String name;
+	public double nPrimeiroTrimestre;
+	public double nSegundoTrimestre;
+	public double nTerceiroTrimestre;
+	
+	public String nomeAluno() {
+		return name;
+	}
+	
+	public double resultadoFinal() {
+		return nPrimeiroTrimestre + nSegundoTrimestre + nTerceiroTrimestre; 
+	}
+	
+	public String toString() {
+		double nota = resultadoFinal();
+		String resultado_final;
+		if (nota < 60.00) {
+			resultado_final = "FAILED";
+		} else {
+			resultado_final = "PASS";
+		}
+		return resultado_final;
+	}
 }
