eb6369486b83231442080d3404f1362c8c986c95
Emerson Costa <ercosta90@gmail.com>
2022-01-27 22:35:40 -0300

    URI 1012

--- ws_eclipse/exercicios_uri/src/URI_1012.java
+++ ws_eclipse/exercicios_uri/src/URI_1012.java
@@ -1,9 +1,63 @@
+/* 
+	Escreva um programa que leia três valores com ponto flutuante de dupla precisão: 
+	A, B e C. Em seguida, calcule e mostre:
+	
+	a) a área do triângulo retângulo que tem A por base e C por altura.
+	b) a área do círculo de raio C. (pi = 3.14159)
+	c) a área do trapézio que tem A e B por bases e C por altura.
+	d) a área do quadrado que tem lado B.
+	e) a área do retângulo que tem lados A e B.
+	Entrada
+	O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+	
+	Saída
+	O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a 
+	uma das áreas descritas acima, sempre com mensagem correspondente e um espaço 
+	entre os dois pontos e o valor. O valor calculado deve ser apresentado com 3 
+	dígitos após o ponto decimal.
+	
+	Exemplo de entrada: 
+		3.0 4.0 5.2
+	Exemplo de saida:
+		TRIANGULO: 7.800
+		CIRCULO: 84.949
+		TRAPEZIO: 18.200
+		QUADRADO: 16.000
+		RETANGULO: 12.000
+*/
+
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class URI_1012 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a, b, c, triangulo, circulo, trapezio, quadrado, retangulo, PI;
+		
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		c = sc.nextDouble();
+		
+		PI = 3.14159;
+		
+		triangulo = a * c / 2; // Base * altura / 2
+		circulo = PI * c * c; // PI * raio elevado a 2
+		trapezio = ((a + b) * c) / 2; // Base menor + base maior * altura / 2
+		quadrado = b * b; // lado * lado ou lado elevado a 2
+		retangulo = a * b; // lado A * lado B
+		
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		System.out.printf("RETANGULO: %.3f%n", retangulo);
+		
+		sc.close();
 	}
 
 }
