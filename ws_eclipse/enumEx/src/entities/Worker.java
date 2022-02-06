dada75d86f05b065f6a368cb0ba8c00500760f0a
Emerson Costa <ercosta90@gmail.com>
2022-02-05 22:18:22 -0300

    Enum

--- ws_eclipse/enumEx/src/entities/Worker.java
+++ ws_eclipse/enumEx/src/entities/Worker.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import entities.enums.WorkerLevel;
@@ -72,7 +73,16 @@ public class Worker {
 	
 	public double income(int year, int month) {
 		double sum = baseSalary;
-		for ()
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
 	}
 }
 
