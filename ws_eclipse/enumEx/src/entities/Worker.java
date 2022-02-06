ab5937cb2bb783b76ef1ee5bd9f2530b07cae942
Emerson Costa <ercosta90@gmail.com>
2022-02-05 21:28:42 -0300

    Enum

--- ws_eclipse/enumEx/src/entities/Worker.java
+++ ws_eclipse/enumEx/src/entities/Worker.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.enums.WorkerLevel;
@@ -11,6 +12,70 @@ public class Worker {
 	private Double baseSalary;
 	
 	private Department department;
-	private List<HourContract> contracts;
+	private List<HourContract> contracts = new ArrayList<>(); // Quando tiver uma composição tem muitos (que no caso é uma lista), 
+															 // não incluir no constructor, simplesmente inicia a lista vazia aqui.
+	public Worker() {
+	}
+
+	public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
 	
+	// implementando os metodos
+	
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		for ()
+	}
 }
+
+
+
+
