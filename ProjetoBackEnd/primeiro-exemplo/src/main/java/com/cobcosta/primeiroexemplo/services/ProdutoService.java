2d2a637f3edb50fcc402b770839f1e12c598942a
Emerson Costa <ercosta90@gmail.com>
2022-02-07 20:54:31 -0300

    CRUD

--- ProjetoBackEnd/primeiro-exemplo/src/main/java/com/cobcosta/primeiroexemplo/services/ProdutoService.java
+++ ProjetoBackEnd/primeiro-exemplo/src/main/java/com/cobcosta/primeiroexemplo/services/ProdutoService.java
@@ -1,6 +1,7 @@
 package com.cobcosta.primeiroexemplo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.cobcosta.primeiroexemplo.model.Produto;
 import com.cobcosta.primeiroexemplo.repository.ProdutoRepository;
@@ -14,7 +15,30 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    /**
+     * Metodo para retornar uma lista de produtos
+     * @return Lista de produtos.
+    */
     public List<Produto> obterTodos(){
         return produtoRepository.obterTodos();
+    }
+
+    /**
+     * Metodo que retorna o produto encontrato pelo seu Id.
+     * @param id do produto que será localizado.
+     * @return Retorna um produto caso seja encontrado.
+    */
+    public Optional<Produto> obterPorId(Integer id){
+        return produtoRepository.obterPorId(id);
+    }
+
+    /**
+     * Metodo para adicionar produto na lista.
+     * @param produto que será adicionado.
+     * @return Retorna o produto que foi adicionado na lista.
+    */
+    public Produto adicionar(Produto produto){
+
+        return produtoRepository.adicionar(produto);
     }
 }
