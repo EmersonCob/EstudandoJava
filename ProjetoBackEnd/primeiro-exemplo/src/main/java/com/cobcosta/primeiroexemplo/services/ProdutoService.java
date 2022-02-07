729dd6fc0f92b0302c8b5d261032a098c070b5a1
Emerson Costa <ercosta90@gmail.com>
2022-02-07 20:50:19 -0300

    CRUD

--- ProjetoBackEnd/primeiro-exemplo/src/main/java/com/cobcosta/primeiroexemplo/services/ProdutoService.java
+++ ProjetoBackEnd/primeiro-exemplo/src/main/java/com/cobcosta/primeiroexemplo/services/ProdutoService.java
@@ -1,5 +1,20 @@
 package com.cobcosta.primeiroexemplo.services;
 
+import java.util.List;
+
+import com.cobcosta.primeiroexemplo.model.Produto;
+import com.cobcosta.primeiroexemplo.repository.ProdutoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoService {
     
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<Produto> obterTodos(){
+        return produtoRepository.obterTodos();
+    }
 }
